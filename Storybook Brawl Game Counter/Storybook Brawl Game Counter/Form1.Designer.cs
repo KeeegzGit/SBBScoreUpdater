8dfaa7d62710e7001bd8e96a09e39839c542abd9
KeeegzGit <88835213+KeeegzGit@users.noreply.github.com>
2021-08-13 00:40:16 +1200

    Add files via upload

---
+++ Storybook Brawl Game Counter/Storybook Brawl Game Counter/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+using System;
+
+namespace Storybook_Brawl_Game_Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButtonAdd = new System.Windows.Forms.RadioButton();
+            this.radioButtonRemove = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Write";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // radioButtonAdd
+            // 
+            this.radioButtonAdd.AutoSize = true;
+            this.radioButtonAdd.Location = new System.Drawing.Point(119, 14);
+            this.radioButtonAdd.Name = "radioButtonAdd";
+            this.radioButtonAdd.Size = new System.Drawing.Size(44, 17);
+            this.radioButtonAdd.TabIndex = 22;
+            this.radioButtonAdd.TabStop = true;
+            this.radioButtonAdd.Text = "Add";
+            this.radioButtonAdd.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRemove
+            // 
+            this.radioButtonRemove.AutoSize = true;
+            this.radioButtonRemove.Location = new System.Drawing.Point(119, 39);
+            this.radioButtonRemove.Name = "radioButtonRemove";
+            this.radioButtonRemove.Size = new System.Drawing.Size(65, 17);
+            this.radioButtonRemove.TabIndex = 23;
+            this.radioButtonRemove.TabStop = true;
+            this.radioButtonRemove.Text = "Remove";
+            this.radioButtonRemove.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 129);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButtonRemove);
+            this.Controls.Add(this.radioButtonAdd);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButtonAdd;
+        private System.Windows.Forms.RadioButton radioButtonRemove;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Storybook Brawl Game Counter/Storybook Brawl Game Counter/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Storybook_Brawl_Game_Counter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a string variable with the selected placement in the combo box
+            string placement = comboBox1.Text;
+            //create an interger to store the number of the placements previously added
+            int number = 0;
+            // Set a variable to the Documents path.
+            string docPath =
+              @"C:\Users\shado\Documents\StorybookBrawlcounter\";
+            // Read the file as one string.
+            string text = File.ReadAllText(docPath + placement + ".txt");
+            //check if the text in the file is an interger
+            bool v = int.TryParse(text, out number);
+            //if text is an interger then store it in an interger variable
+            if (v == true)
+            {
+                //changing the previous amount of placements into an interger
+                int numplace = int.Parse(text);
+                //adding 1 to the text file
+                if (radioButtonAdd.Checked == true)
+                {
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, placement + ".txt")))
+                    {
+                        outputFile.WriteLine(numplace + 1);
+                    }
+                }
+                else
+                {
+                    if (radioButtonRemove.Checked == true)
+                    {
+                        using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, placement + ".txt")))
+                        {
+                            outputFile.WriteLine(numplace - 1);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select add or remove");
+                    }
+                } 
+            }
+            else 
+            {
+                MessageBox.Show("Text in file" + placement + ".txt is not in a valid format");
+            }
+            // Write the string array to a new file named "WriteLines.txt".
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //get the date and time
+            DateTime Now = DateTime.Now;
+            // Set a variable to the Documents path.
+            string docPath =
+              @"C:\Users\shado\Documents\StorybookBrawlcounter\";
+            //add info in documents to array
+            string[] lines =  
+            { 
+                File.ReadAllText(docPath + "1.txt"),
+                File.ReadAllText(docPath + "2.txt"),
+                File.ReadAllText(docPath + "3.txt"),
+                File.ReadAllText(docPath + "4.txt"),
+                File.ReadAllText(docPath + "5.txt"),
+                File.ReadAllText(docPath + "6.txt"),
+                File.ReadAllText(docPath + "7.txt"),
+                File.ReadAllText(docPath + "8.txt"),
+            };
+            //writes the current scores to a file with todays date
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, Now.ToString("yyyyMMdd") + ".txt")))
+            {
+                outputFile.WriteLine(Now);                
+                foreach(string line in lines)
+                {
+                    outputFile.WriteLine(line);
+                }
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //create an interger
+            int i;
+            // Set a variable to the Documents path.
+            string docPath =
+              @"C:\Users\shado\Documents\StorybookBrawlcounter\";
+            //clear the contents of each text document
+            for(i = 1; i < 9; i++)
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, i + ".txt")))
+            {
+                outputFile.WriteLine(0);
+            }
+        }
+    }
+}
